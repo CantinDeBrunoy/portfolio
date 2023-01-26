047e7baf722974bd3b63e23c4500bace9d6fee27
CantinDeBrunoy <cantin.roquier@gmail.com>
2023-01-26 11:01:34 +0100

    style(HomePage): style button mesprojets

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,36 @@
 import './HomePage.css'
 import paralax from '../plugin/paralax.js'
 import scrollDown from '../img/scrolldown.gif'
+import { useEffect, useState } from 'react';
 
 const HomePage = () => {
+    const [indexY,setIndexY] = useState();
+    const [buttonIsClicked,setButtonIsClicked] = useState(false);
 
+    const handleScroll = () => {
+        const scrollPosition = window.scrollY; 
+        setIndexY(scrollPosition);
+    };
+
+    useEffect(() => {
+        //handleScroll();
+        setIndexY(window.scrollY);
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+          window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const onClickmyProjectButton = (bool) => {
+        setButtonIsClicked(bool);
+    }
 
   return (
     <div className="HomePage">
         <div className="paralax">
             <div className="paralax-item p-item1"></div>
             <div className="paralax-item p-item2"></div>
-            <div className="paralax-item p-item3"></div>
+            <div className={`paralax-item p-item3 ${indexY > 1100 ? 'LowerZindex' : ''}`}></div>
         </div>
 
         <div className="container">
@@ -23,8 +43,9 @@ const HomePage = () => {
                 <h2>Développeur web</h2>
             </div>
 
-            <div className="content">
-                <button className='button-29'>BUTTON</button>
+            <div className={`content `}>
+                    <button className='button' onClick={()=> onClickmyProjectButton(true)}>MES PROJETS</button>
+                    <div className={`backgroundTransition ${buttonIsClicked ? 'displayBlock': ''}`}></div>
             </div>
             
 
