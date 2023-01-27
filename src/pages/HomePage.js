9791202ed9e0db1bdad57a087ccadd7db57f30d7
CantinDeBrunoy <cantin.roquier@gmail.com>
2023-01-27 19:40:00 +0100

    style(HomePage) : animation homepage button

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import './HomePage.css'
 import paralax from '../plugin/paralax.js'
-import scrollDown from '../img/scrolldown.gif'
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { homePageTradEng, homePageTradFR } from '../trad/homePageFormat';
 
 const HomePage = () => {
     const [indexY,setIndexY] = useState();
     const [buttonIsClicked,setButtonIsClicked] = useState(false);
+    const [format,setFormat] = useState(homePageTradEng);
+    const navigate = useNavigate();
 
     const handleScroll = () => {
         const scrollPosition = window.scrollY; 
@@ -23,10 +26,23 @@ const HomePage = () => {
 
     const onClickmyProjectButton = (bool) => {
         setButtonIsClicked(bool);
+        setTimeout(() => {
+            navigate('projects');
+        }, "5000")
+        setTimeout(() => {
+            setButtonIsClicked(!bool);
+        }, "12000")
+          
+    }
+
+    const switchLanguage = () => {
+        setFormat(format == homePageTradFR ?  homePageTradEng: homePageTradFR);
     }
 
   return (
     <div className="HomePage">
+        <div className='LangButton' onClick={switchLanguage}></div>
+
         <div className="paralax">
             <div className="paralax-item p-item1"></div>
             <div className="paralax-item p-item2"></div>
@@ -35,17 +51,19 @@ const HomePage = () => {
 
         <div className="container">
             <div className="content-top">
-                <h1>SCROLL VERS LE BAS</h1>
+                <h1>{format.scroll}</h1>
             </div>
                        
             <div className="content-header">
-                <h1>Cantin ROQUIER</h1>
-                <h2>Développeur web</h2>
+                <h1>{format.name}</h1>
+                <h2>{format.subtitle}</h2>
             </div>
 
             <div className={`content `}>
-                    <button className='button' onClick={()=> onClickmyProjectButton(true)}>MES PROJETS</button>
-                    <div className={`backgroundTransition ${buttonIsClicked ? 'displayBlock': ''}`}></div>
+                    <button className='button' onClick={()=> onClickmyProjectButton(true)}>{format.projects}</button>
+                    <div className={`backgroundTransition TransitionBlue ${buttonIsClicked ? 'displayBlock': ''}`}></div>
+                    <div className={`backgroundTransition TransitionPurple ${buttonIsClicked ? 'displayBlock': ''}`}></div>
+                    <div className={`backgroundTransition TransitionBlack ${buttonIsClicked ? 'displayBlock': ''}`}></div>
             </div>
             
 
---
+++ src/trad/homePageFormat.js
@@ -0,0 +1,13 @@
+export const homePageTradFR = {
+    scroll:'SCROLL VERS LE BAS',
+    name: 'CANTIN ROQUIER',
+    subtitle:'Développeur web',
+    projects:'MES PROJETS'
+}
+
+export const homePageTradEng = {
+    scroll:'SCROLL DOWN',
+    name: 'CANTIN ROQUIER',
+    subtitle:'web Developer ',
+    projects:'MY PROJECTS'
+}
